2a4c76129282c791ee51caa2a7df689f2c9b6507
Yuri Klind <vykmnsk@gmail.com>
2013-06-13 22:04:14 +1000

    add checking current price, lookup confirm textbox and btn by css

--- ebaybidder.py
+++ ebaybidder.py
@@ -1,11 +1,12 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
 from datetime import datetime
 import ebaybidder_cfg as cfg
 from dbg import set_trace
 import time
+import re
 
 
 def calc_secs_left(endtime_text):
@@ -16,16 +17,50 @@ def calc_secs_left(endtime_text):
     return secsleft
 
 
-def parse_mins_secs_left(timetext):
-    mins, secs = 0, 0
-    timetext = timetext.replace('m', '').replace('s', '')
-    timenums = timetext.split()
-    if len(timenums) == 2:
-        mins, secs = timenums
-    elif len(timenums) == 1:
-        secs = timenums[0]
-    secs = 60 * int(mins) + int(secs)
-    return secs
+def parse_time_text(timetext):
+    matches = re.split('[d|h|m|s]', timetext)
+    nums = [int(m.strip()) for m in matches if m]
+    padded = []
+    for _ in range(len('dhms') - len(nums)):
+        padded.append(0)
+    padded.extend(nums)
+    return padded
+
+
+def convert2secs(time_pieces):
+    total_secs = 0
+    for pair in zip([24*60*60, 60*60, 60, 1], time_pieces):
+        total_secs += pair[0] * pair[1]
+    return total_secs
+
+
+def read_calc_verify_timeleft():
+    endtime_el = browser.find_element_by_css_selector("span.vi-tm-left")
+    endtime_text = endtime_el.text
+    secsleft_calced = calc_secs_left(endtime_text)
+    print 'secsleft_calced=', secsleft_calced
+
+    timeleft_text = browser.find_element_by_css_selector("span.tmlHt").text
+    time_pieces = parse_time_text(timeleft_text)
+    secsleft_shown = convert2secs(time_pieces)
+    print 'secsleft_shown=', secsleft_shown
+
+    assert abs(secsleft_calced - secsleft_shown) < 100, \
+        'time left shown and calculated mismath!'
+    return min(secsleft_calced, secsleft_shown), endtime_text
+
+
+def read_price():
+    price_el = browser.find_element_by_id('w1-15-_bidPrice')
+    price_text = price_el.text.strip()
+    m = re.search('.+\$(\d+\.\d\d)', price_text)
+    if m:
+        price = float(m.groups()[0])
+        print 'current price: %s:' % price_text
+    else:
+        print 'could not parse the price: %s' % price_text
+        price = -1
+    return price
 
 
 def bid(browser, maxbid):
@@ -33,34 +68,37 @@ def bid(browser, maxbid):
     bidbutton = browser.find_element_by_id('bidBtn_btn')
     bidbox.send_keys(str(maxbid))
     bidbutton.click()
-    bidbox2 = browser.find_element_by_id('w1-28-_enter_val')
-    bidbutton2 = browser.find_element_by_id('w1-28-_place_btn')
+    # bidbox2 = browser.find_element_by_id('w1-24-_enter_val')
+    bidbox2 = browser.find_element_by_css_selector("div.maxbidinput input[type='text']")
+    # bidbutton2 = browser.find_element_by_id('w1-24-_place_btn')
+    bidbutton2 = browser.find_element_by_css_selector("table.btnLnk input[type='button']")
     bidbox2.send_keys(str(maxbid))
     bidbutton2.click()
     set_trace()
 
+
 def main(browser):
     browser.get(cfg.url)
-    endtime_el = browser.find_element_by_css_selector("span.vi-tm-left")
-    endtime_text = endtime_el.text
-    secsleft_calced = calc_secs_left(endtime_text)
-    hibernate_secs = secsleft_calced - cfg.hibernate_until
+    secsleft, endtime_text = read_calc_verify_timeleft()
+    hibernate_secs = secsleft - cfg.hibernate_until
     if hibernate_secs > cfg.hibernate_min:
-        # browser.close()
         print "too long until %s, will hibernate for %d mins" \
             % (endtime_text, hibernate_secs / 60)
         time.sleep(hibernate_secs)
         print "waking up!"
+        browser.get(cfg.url)
+        secsleft, endtime_text = read_calc_verify_timeleft()
 
-    browser.get(cfg.url)
     for _ in xrange(100):
-        timeleft_text = browser.find_element_by_css_selector("span.tmlHt").text
-        secsleft_shown = parse_mins_secs_left(timeleft_text)
         secsleft_calced = calc_secs_left(endtime_text)
-        print 'secsleft_shown=', secsleft_shown
-        print 'secsleft_calced=', secsleft_calced
-        secs_left = min(secsleft_shown, secsleft_calced)
-        secs2bid = secs_left - cfg.bid_when_left
+        secs2bid = secsleft_calced - cfg.bid_when_left
+        # TODO check, print current price
+        price = read_price()
+        maxprice = float(cfg.maxbid)
+        if price > maxprice:
+            print 'current price=%.2f is already higher that max bid=%.2f' \
+                % (price, maxprice)
+
         if secs2bid < 2:
             print 'Bid!!!'
             bid(browser, cfg.maxbid)
@@ -76,5 +114,5 @@ if __name__ == '__main__':
     browser.implicitly_wait(15)
     try:
         main(browser)
-    except:
+    finally:
         browser.close()
--- ebaybidder_cfg.py
+++ ebaybidder_cfg.py
@@ -1,5 +1,5 @@
-url = 'http://www.ebay.com.au/itm/2010-1-5TH-TUVALU-TIGER-GOLD-PROOF-COIN-VERY-SCARCE-/221234432591?pt=AU_Coins2&hash=item3382998a4f'
-hibernate_until = 15 * 60
-bid_when_left = 120
+url = 'http://www.ebay.com....'
+hibernate_until = 60 * 60
+bid_when_left = 1200
 hibernate_min = 5 * 60
 maxbid = 1.25
