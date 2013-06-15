7c0159782225bce81c886be2722e6d854214dd59
Yuri Klind <vykmnsk@gmail.com>
2013-06-15 21:05:27 +1000

    add login, overall works, missing reading msg afterbid

--- ebaybidder.py
+++ ebaybidder.py
@@ -1,12 +1,10 @@
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from datetime import datetime
 import ebaybidder_cfg as cfg
-from dbg import set_trace
 import time
 import re
+import os
 
 
 def calc_secs_left(endtime_text):
@@ -51,8 +49,8 @@ def read_calc_verify_timeleft():
 
 
 def read_price():
-    price_el = browser.find_element_by_id('w1-15-_bidPrice')
-    price_text = price_el.text.strip()
+    price_el = browser.find_element_by_css_selector("div.actPanel span[itemprop='price']")
+    price_text = price_el.text.strip().replace(',', '')
     m = re.search('.+\$(\d+\.\d\d)', price_text)
     if m:
         price = float(m.groups()[0])
@@ -63,18 +61,38 @@ def read_price():
     return price
 
 
+def login(browser):
+    usr = os.environ['EBAYUSR']
+    pwd = os.environ['EBAYPWD']
+    if not usr or not pwd:
+        print 'CANNOT LOGIN: env vars EBAYUSR and/or EBAYPWD not set!'
+    signin_link = browser.find_element_by_link_text('Sign in')
+    signin_link.click()
+    usr_textbox = browser.find_element_by_id('userid')
+    pwd_textbox = browser.find_element_by_id('pass')
+    sign_btn = browser.find_element_by_id('sgnBt')
+    usr_textbox.send_keys(usr)
+    pwd_textbox.send_keys(pwd)
+    sign_btn.submit()
+    return
+
+
 def bid(browser, maxbid):
     bidbox = browser.find_element_by_id('MaxBidId')
     bidbutton = browser.find_element_by_id('bidBtn_btn')
     bidbox.send_keys(str(maxbid))
     bidbutton.click()
     # bidbox2 = browser.find_element_by_id('w1-24-_enter_val')
-    bidbox2 = browser.find_element_by_css_selector("div.maxbidinput input[type='text']")
+    bidbox2 = browser.find_element_by_css_selector("div.maxbidinput span input[type='text']")
     # bidbutton2 = browser.find_element_by_id('w1-24-_place_btn')
     bidbutton2 = browser.find_element_by_css_selector("table.btnLnk input[type='button']")
     bidbox2.send_keys(str(maxbid))
     bidbutton2.click()
-    set_trace()
+
+
+def read_msg(browser):
+    msg_el = browser.find_element_by_css_selector('div.sm-t div')
+    return msg_el.text
 
 
 def main(browser):
@@ -89,6 +107,10 @@ def main(browser):
         browser.get(cfg.url)
         secsleft, endtime_text = read_calc_verify_timeleft()
 
+    print 'Logging in...'
+    login(browser)
+    print 'logged in!'
+
     for _ in xrange(100):
         secsleft_calced = calc_secs_left(endtime_text)
         secs2bid = secsleft_calced - cfg.bid_when_left
@@ -102,6 +124,7 @@ def main(browser):
         if secs2bid < 2:
             print 'Bid!!!'
             bid(browser, cfg.maxbid)
+            print read_msg(browser)
             break
         else:
             waitfor = secs2bid / 2
--- ebaybidder_cfg.py
+++ ebaybidder_cfg.py
@@ -1,5 +1,6 @@
-url = 'http://www.ebay.com....'
-hibernate_until = 60 * 60
-bid_when_left = 1200
+# url = 'http://www.ebay.com/itm/1907-Gold-Sovereign-Full-/290931232788?&autorefresh=true'
+url = 'http://www.ebay.com.au/itm/Country-Road-New-Wool-Cotton-Blend-Scarf-BNWT-39-95-Charcoal-With-Gold-Thread-/200933687866?pt=AU_Women_Accessories&hash=item2ec894c23a&_uhb=1'
+hibernate_until = 30 * 60
+bid_when_left = 600
 hibernate_min = 5 * 60
 maxbid = 1.25
